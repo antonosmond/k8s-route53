88b8c9a524816d1b154431ecc0b17163b5b08eb4
Anton Osmond <antonosmond@gmail.com>
2016-08-10 11:14:24 +0100

    Changed to use k8s REST API and removed need for kubectl

--- run.sh
+++ run.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 set -e
 
-REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+# Get the kubernetes API bearer token
+KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 
-SVC=$(kubectl --namespace "${SERVICE_NAMESPACE:-default}" describe svc "$SERVICE_NAME")
-HOSTED_ZONE_DOMAIN=$(aws route53 get-hosted-zone --id "$HOSTED_ZONE_ID" | jq -r .HostedZone.Name)
-DNS_NAME=$(echo "$DNS_NAME" | sed "s/.$HOSTED_ZONE_DOMAIN//")
-DNS_NAME="${DNS_NAME}.${HOSTED_ZONE_DOMAIN}"
-ELB_DNS=$(echo "$SVC" | grep 'LoadBalancer Ingress' | awk '{ print $3 }')
+# Get the AWS ELB details
+REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+ELB_DNS=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
+  "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/kube-system/services/$SERVICE_NAME" | \
+  jq -r '.status.loadBalancer.ingress[0].hostname')
 ELB_NAME=$(echo "$ELB_DNS" | awk -F '-' '{ print $2 }')
 ELB_HOSTED_ZONE_ID=$(aws --region "$REGION" elb describe-load-balancers --load-balancer-name "$ELB_NAME" | jq -r .LoadBalancerDescriptions[0].CanonicalHostedZoneNameID)
 
+# Generate the FQDN for the requested DNS name
+HOSTED_ZONE_DOMAIN=$(aws route53 get-hosted-zone --id "$HOSTED_ZONE_ID" | jq -r .HostedZone.Name)
+DNS_NAME=$(echo "$DNS_NAME" | sed "s/.$HOSTED_ZONE_DOMAIN//")
+DNS_NAME="${DNS_NAME}.${HOSTED_ZONE_DOMAIN}"
+
+# Create the change batch record to add the DNS entry in the given AWS hosted zone
 CHANGE_BATCH="{
   \"Changes\": [
     {
@@ -28,4 +35,5 @@ CHANGE_BATCH="{
   ]
 }"
 
+# Submit the change batch request
 aws route53 change-resource-record-sets --hosted-zone-id "$HOSTED_ZONE_ID" --change-batch "$CHANGE_BATCH"
