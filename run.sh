21929132d0951c2bfeef1b52b53e9cc6c7734d1b
Anton Osmond <antonosmond@gmail.com>
2016-08-10 21:55:12 +0100

    Changed to get Hosted zone ID from FQDN

--- run.sh
+++ run.sh
@@ -1,21 +1,19 @@
 #!/bin/bash
 set -e
 
-# Get the kubernetes API bearer token
-KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+# Get the hosted zone ID for the requested FQDN
+FQDN=$(echo "$FQDN" | sed 's|\.*$|.|')
+HOSTED_ZONE_ID=$(aws route53 list-hosted-zones | \
+  jq -r --arg FQDN "$FQDN" '.HostedZones | map(select(.Name | inside($FQDN))) | max_by(.Name | length) | .Id | ltrimstr("/hostedzone/")')
 
 # Get the AWS ELB details
 REGION=$(curl -s 169.254.169.254/latest/dynamic/instance-identity/document | jq -r .region)
+KUBE_TOKEN=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
 ELB_DNS=$(curl -sSk -H "Authorization: Bearer $KUBE_TOKEN" \
   "https://$KUBERNETES_SERVICE_HOST:$KUBERNETES_SERVICE_PORT/api/v1/namespaces/kube-system/services/$SERVICE_NAME" | \
   jq -r '.status.loadBalancer.ingress[0].hostname')
 ELB_NAME=$(echo "$ELB_DNS" | awk -F '-' '{ print $2 }')
 ELB_HOSTED_ZONE_ID=$(aws --region "$REGION" elb describe-load-balancers --load-balancer-name "$ELB_NAME" | jq -r .LoadBalancerDescriptions[0].CanonicalHostedZoneNameID)
-
-# Generate the FQDN for the requested DNS name
-HOSTED_ZONE_DOMAIN=$(aws route53 get-hosted-zone --id "$HOSTED_ZONE_ID" | jq -r .HostedZone.Name)
-DNS_NAME=$(echo "$DNS_NAME" | sed "s/.$HOSTED_ZONE_DOMAIN//")
-DNS_NAME="${DNS_NAME}.${HOSTED_ZONE_DOMAIN}"
 
 # Create the change batch record to add the DNS entry in the given AWS hosted zone
 CHANGE_BATCH="{
@@ -23,7 +21,7 @@ CHANGE_BATCH="{
     {
       \"Action\": \"UPSERT\",
       \"ResourceRecordSet\": {
-        \"Name\": \"$DNS_NAME\",
+        \"Name\": \"$FQDN\",
         \"Type\": \"A\",
         \"AliasTarget\": {
           \"HostedZoneId\": \"$ELB_HOSTED_ZONE_ID\",
